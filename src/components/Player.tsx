6a949a2867958e3118b9db1cc35e549198d4c091
Liam Darkmoon <liamdarkmoon@gmail.com>
2025-05-06 11:17:51 -0300

    Fixed: music

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,25 +5,20 @@ export default function Player() {
     const audioRef = useRef<HTMLAudioElement>(null);
     const [isPlaying, setIsPlaying] = useState(true);
 
-   useEffect(() => {
-       if (audioRef.current) {
-           setIsPlaying(!audioRef.current.paused)
-
-           if (isPlaying) {
-               audioRef.current.play();
-           } else {
-               audioRef.current.pause();
-           }
-       }
-   },[isPlaying])
+   
 
     const HandlePlay = () => {
         setIsPlaying(!isPlaying);
+        if(isPlaying) {
+            audioRef.current?.pause();
+        } else {
+            audioRef.current?.play();
+        }
     }
 
     return(
         <>
-            <audio ref={audioRef} src="/turning.mp3" autoPlay loop/>
+            <audio ref={audioRef} src="/song.m4a" autoPlay={true} preload="/song.m4a" loop/>
 
             <div onClick={HandlePlay} className="fixed right-0 top-[100px] z-999 bg-rose-50 rounded-tl-full rounded-bl-full p-2 pr-6 shadow-md shadow-rose-900/80">
                 <span className="bg-rose-900/80 rounded-full p-1.5 cursor-pointer w-[50px] h-[50px] grid place-items-center">
